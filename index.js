580735b54929a1ee7d801e8128c66139ac6b6157
shl422 <h.k.domingo@outlook.com>
2021-08-02 10:43:55 +0800

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+let sum = '19';
+sum + 3;
+
+let difference = '25';
+difference - 3;
+
+let product = '11';
+product * 2;
+
+let dividend = '44';
+dividend / 2;
+
+
+function add(a,b) {
+    return a + b
+  }
+  
+function subtract(a,b) {
+    return a - b
+  }
+  
+function divide(a,b) {
+    return a / b
+  }
+  
+function multiply(a,b) {
+    return a * b
+  }
+  
+function increment(n) {
+    return n += 1
+  }
+  
+function decrement(n) {
+    return n -= 1
+  }
+  
+function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+function preserveDecimal(string) {
+    return parseFloat(string)
+  }
+  
+
